6739f518b6c9dc2ed0912df7761079bd426d8453
Jacob Carse <jacob.carse@gmail.com>
2020-08-19 01:00:21 +0100

    Testing Loop

--- train.py
+++ train.py
@@ -3,10 +3,12 @@
 """
 This file contains implementations of the functions used to train the multi directional CPC model:
     train_cpc - Function used to facilitate the training of the Multi Directional Contrastive Predictive Coding model.
+    test_cpc - Function used for the testing of training Multi Directional Contrastive Predictive Coding model.
 """
 
 
 # Built-in/Generic Imports
+import os
 import time
 
 # Library Imports
@@ -69,12 +71,13 @@ def train_cpc(arguments, device):
                                                shuffle=True, num_workers=arguments["data_workers"],
                                                pin_memory=False, drop_last=True)
 
+    # Calculates the number of patches across each dimension.
     in_patches = int(((arguments["image_size"] + 2 * 0 - 1 * (arguments["cpc_patch_size"] - 1) - 1)
                       / arguments["cpc_patch_stride"]) + 1)
 
     log(arguments, "Loaded Datasets")
 
-    # Initialises the encode and autoregressor.
+    # Initialises the encoder and autoregressor.
     encoder = EfficientNetEncoder(arguments["efficient_net_b"], arguments["cpc_code_size"])
     autoregressor = MultiDirectionalPixelCNN(arguments["cpc_code_size"],
                                              multi_directional=arguments["cpc_multi_directional"])
@@ -95,7 +98,8 @@ def train_cpc(arguments, device):
 
     # If 16 bit precision is being used change the model and optimisers precision.
     if arguments["precision"] == 16:
-        [encoder, autoregressor], optimiser = amp.initialize([encoder, autoregressor], opt_lebel="O2", verbosity=False)
+        [encoder, autoregressor], optimiser = amp.initialize([encoder, autoregressor], optimiser,
+                                                             opt_lebel="O2", verbosity=False)
 
     # Checks if precision level is supported and if not defaults to 32.
     elif arguments["precision"] != 32:
@@ -186,7 +190,7 @@ def train_cpc(arguments, device):
                 batch_losses.append(-log_softmax[0, ])
 
             # Finds the loss of the batch by combinding the loss for each image in the batch.
-            loss = torch.sum(torch.cat(batch_losses))
+            loss = torch.mean(torch.cat(batch_losses))
 
             # Backward propagates the loss over the model.
             if arguments["precision"] == 16:
@@ -301,7 +305,7 @@ def train_cpc(arguments, device):
                     batch_losses.append(-log_softmax[0,])
 
                 # Combines the loss for each image into a batch loss
-                validation_loss += torch.sum(torch.cat(batch_losses))
+                validation_loss += torch.mean(torch.cat(batch_losses))
                 validation_batches += 1
 
         # Writes the validation loss to TensorBoard
@@ -349,3 +353,151 @@ def train_cpc(arguments, device):
 
     # Returns the loss values from the training.
     return losses, validation_losses, best_epoch
+
+
+def test_cpc(arguments, device):
+    """
+    Function used to test a trained Multi-Directional Contrastive Predictive Coding model.
+    :param arguments: Dictionary containing arguments.
+    :param device: PyTorch device object.
+    :return Returns the loss for the testing data on the model.
+    """
+
+    # Loads the testing dataset.
+    test_data = Dataset(arguments, "test")
+
+    # Creates the data loader for the testing data.
+    test_data_loader = DataLoader(test_data, batch_size=arguments["batch_size"],
+                                          shuffle=False, num_workers=arguments["data_workers"],
+                                          pin_memory=False, drop_last=True)
+
+    # Shuffles a copy of the testing data.
+    random_test_data = test_data.shuffle()
+
+    # Creates the data loader for the random testing data.
+    random_test_loader = DataLoader(random_test_data, batch_size=arguments["random_patches"],
+                                    shuffle=True, num_workers=arguments["data_workers"],
+                                    pin_memory=False, drop_last=True)
+
+    # Calculates the number of patches across each dimension.
+    in_patches = int(((arguments["image_size"] + 2 * 0 - 1 * (arguments["cpc_patch_size"] - 1) - 1)
+                      / arguments["cpc_patch_stride"]) + 1)
+
+    log(arguments, "Loaded Testing Data")
+
+    # Initialises the encoder and autoregressor.
+    encoder = EfficientNetEncoder(arguments["efficient_net_b"], arguments["cpc_code_size"])
+    autoregressor = MultiDirectionalPixelCNN(arguments["cpc_code_size"],
+                                             multi_directional=arguments["cpc_multi_directional"])
+
+    # Loads the trained weights of the encoder and autoregressor.
+    encoder.load_state_dict(torch.load(os.path.join(arguments["model_dir"], arguments["experiment"] +
+                                                    "_encoder_best.pt"), map_location=device))
+    autoregressor.load_state_dict(torch.load(os.path.join(arguments["model_dir"], arguments["experiment"] +
+                                                          "_autoregressor_best.pt"), map_location=device))
+
+    # Sets the models to evaluation mode.
+    encoder.eval()
+    autoregressor.eval()
+
+    # Moves the models to the selected device.
+    encoder.to(device)
+    autoregressor.to(device)
+
+    # If 16 bit precision is being used change the model and optimisers precision.
+    if arguments["precision"] == 16:
+        [encoder, autoregressor] = amp.initialize([encoder, autoregressor], opt_lebel="O2", verbosity=False)
+
+    # Checks if precision level is supported and if not defaults to 32.
+    elif arguments["precision"] != 32:
+        log(arguments, "Only 16 and 32 bit precision supported. Defaulting to 32 bit precision.")
+
+    log(arguments, "Models Initialised")
+
+    # Performs a testing epoch with no gradients.
+    with torch.no_grad():
+
+        # Logging metrics for the testing epoch.
+        loss, num_batches = 0, 0
+
+        # Loops through the testing dataset.
+        for batch, _ in test_data_loader:
+            batch_losses = []
+
+            # Moves the batch to the selected device and splits the images to patches.
+            batch = batch.to(device)
+            batch = extract_patches(arguments, batch, in_patches)
+
+            # Encodes the patches with the encoder.
+            encoded_batch = encoder.forward(batch)
+            encoded_batch = encoded_batch.view(arguments["cpc_batch_size"], in_patches, in_patches, -1)
+            encoded_batch = encoded_batch.permute(0, 3, 1, 2)
+
+            # Loads the random patches into memory and splits into patches.
+            random_batch, _ = next(iter(random_test_loader))
+            random_batch = random_batch.to(device)
+            random_batch = extract_patches(arguments, random_batch, in_patches)
+
+            # Encodes the random patches with the encoder.
+            random_encoded = encoder.forward(random_batch)
+            random_encoded = random_encoded.view(arguments["cpc_random_patches"], in_patches, in_patches, -1)
+            random_encoded = random_encoded.permute(0, 3, 1, 2)
+
+            # Clones the encoded batch for masking.
+            masked_batch = encoded_batch.clone()
+
+            # Applies a mask to the encoded batch.
+            if arguments["cpc_alt_mask"]:
+                for i in range(1, 6):
+                    for j in range(1, 6):
+                        masked_batch[:, :, i, j] = 0
+            else:
+                for i in range(3, 7):
+                    for j in range(0, 7):
+                        masked_batch[:, :, i, j] = 0
+
+            # Forward propagates the autoregressor with the masked batch.
+            predictions = autoregressor.forward(masked_batch)
+
+            # Loops through the images in the batch.
+            for image in range(arguments["cpc_batch_size"]):
+
+                # Gets the masked elements for the random patches.
+                if arguments["cpc_alt_mask"]:
+                    predicted_patches = predictions[image, :, 1:6, 1:6].reshape(1, -1)
+                    target_patches = encoded_batch[image, :, 1:6, 1:6].reshape(1, -1)
+                else:
+                    predicted_patches = predictions[image, :, 3:7, 0:7].reshape(1, -1)
+                    target_patches = encoded_batch[image, :, 3:7, 0:7].reshape(1, -1)
+
+                # Calculates the dot terms for the predicted patches.
+                good_dot_terms = torch.sum(predicted_patches * target_patches, dim=1)
+                dot_terms = [torch.unsqueeze(good_dot_terms, dim=0)]
+
+                # Loops through the random images for each batch.
+                for random_image in range(arguments["cpc_random_patches"]):
+
+                    # Gets the masked elements for the random patches.
+                    if arguments["cpc_alt_mask"]:
+                        random_patches = random_encoded[random_image, :, 1:6, 1:6].reshape(1, -1)
+                    else:
+                        random_patches = random_encoded[random_image, :, 3:7, 0:7].reshape(1, -1)
+
+                    # Calculates the dot terms for the random patches.
+                    bad_dot_terms = torch.sum(predicted_patches * random_patches, dim=1)
+                    dot_terms.append(torch.unsqueeze(bad_dot_terms, dim=0))
+
+                # Calculates the log softmax for all the dot terms.
+                log_softmax = torch.log_softmax(torch.cat(dot_terms, dim=0), dim=0)
+                batch_losses.append(-log_softmax[0,])
+
+            # Combines the loss for each image into a batch loss.
+            loss += torch.mean(torch.cat(batch_losses))
+            num_batches += 1
+
+    # Gets the testing loss from the batch losses.
+    loss /= num_batches
+    log(arguments, "\nTesting Loss: {:.6f}".format(loss))
+
+    # Returns the testing loss.
+    return loss
