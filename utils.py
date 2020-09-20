5e55300100b4f04a21307f3f79ecc3ebe7ff7672
Jacob Carse <jacob.carse@gmail.com>
2020-09-20 18:41:43 +0100

    Created HDF5 File Handler

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ This file contains the following utility functions for the application:
     log - Function to both print and log a given input message.
     set_random_seed - Function that sets a seed for all random number generation functions.
     get_device - Function to get the device that will be used.
+    HDF5Handler - Class for the handling of HDF5 files.
 """
 
 
@@ -14,6 +15,7 @@ import os
 import random
 
 # Library Imports
+import h5py
 import torch
 import numpy as np
 from argparse import ArgumentTypeError
@@ -108,3 +110,61 @@ def get_device(arguments):
             return torch.device(f"cuda:{torch.cuda.device_count() - 1}")
         else:
             return torch.device(f"cuda:{arguments['gpu']}")
+
+
+class HDF5Handler(object):
+    """
+    A class for handling of a HDF5 file including, creation and data appending.
+        init - Initialiser for the handler and also creates the HDF5 file.
+        append - Append values from a one dimensional array.
+    """
+
+    def __init__(self, data_path, data_set, shape, data_type=np.float32, compression="gzip", chunk_len=1):
+        """
+        Initialiser for the HDF5 handler which creates the file with the given arguments.
+        :param data_path: String for the file path where the HDF5 file will be stored.
+        :param data_set: The initial data set for the HDF5 file.
+        :param shape: The shape of the HDF5 file.
+        :param data_type: The data type of the HDF5 file.
+        :param compression: The compression used for the HDF5 file.
+        :param chunk_len: The chunk length of the HDF5 file.
+        """
+
+        # Saved the inital parameters into the handler object.
+        self.data_path = data_path
+        self.data_set = data_set
+        self.shape = shape
+        self.i = 0
+
+        # Creates the HDF5 file.
+        with h5py.File(self.data_path, mode='w') as h5_file:
+            self.data = h5_file.create_dataset(data_set,
+                                               shape=(0, ) + shape,
+                                               maxshape=(None, ) + shape,
+                                               dtype=data_type,
+                                               compression=compression,
+                                               chunks=(chunk_len, ) + shape)
+
+    def append(self, values):
+        """
+        Appends an array of values to a HDF5 file.
+        :param values: A list type object of values to be added to the HDF5 file.
+        """
+
+        # Loads the HDF5 file in append mode.
+        with h5py.File(self.data_path, mode='a') as h5_file:
+
+            # Gets the data from the HDF5 file.
+            data = h5_file[self.data_set]
+
+            # Realizes the HDF5 data.
+            data.resize((self.i + len(values), ) + self.shape)
+
+            # Adds the values to the data.
+            data[self.i: self.i + len(values)] = values
+
+            # Increases the length of the HDF5 data.
+            self.i += len(values)
+
+            # Flush the HDF5 file's buffers.
+            h5_file.flush()
